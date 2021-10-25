4932e6090b42b4813dc8f5edf954b8549d8f9358
Leonlav <leonlav77@gmail.com>
2021-10-25 22:49:57 +0200

    daily test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
+use App\Models\Day;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,6 +28,30 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            DB::table('test')->delete();
+            for ($i = 0; $i < 8; $i++) {
+                sleep(1);
+                $this->lastRecord = Day::latest('created_at')->first();
+                $this->newTime = $this->lastRecord->vrijeme + 2;
+                $this->newDate = Carbon::parse($this->lastRecord->date)->addDay();
+                $this->newDate = $this->newDate->format('Y-m-d');
+    
+                $this->today = $this->lastRecord->date;
+                if($this->newTime == 24) {
+                    $this->newTime = 8;
+                    $this->today = $this->newDate;
+                }
+                DB::table('days')->insert([
+                    'date' => $this->today,
+                    'vrijeme' => $this->newTime,
+                    'status' => 'free',
+                    'created_at' => now()
+                ]);
+                
+            }
+
+        })->everyMinute();
     }
 
     /**
